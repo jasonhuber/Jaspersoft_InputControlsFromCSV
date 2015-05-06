e1a1674171c43d7f3d598628b0a43aee8f12ff2f
Jason Huber <jasonhuber@gmail.com>
2015-05-06 11:50:40 -0700

    Added all files
    Be sure to check IP addresses

---
+++ js/config.js
@@ -0,0 +1,8 @@
+var _server = "172.16.252.132";
+var _port = "8080";
+var _url = "http://" + _server + ":" + _port;
+var _installdir = "jasperserver-pro601";
+var _username = "superuser";
+var _password = "superuser";
+var _debug = true;
+var _assert = false;
