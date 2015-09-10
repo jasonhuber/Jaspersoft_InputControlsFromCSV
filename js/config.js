e16addfbd2c823ef6e91fe29e4a8e903c5c7b0a2
Jason Huber <jasonhuber@gmail.com>
2015-09-10 11:29:47 -0700

    updated some stuff

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var _server = "localhost";
 var _port = "8080";
 var _url = "http://" + _server + ":" + _port;
-var _installdir = "jasperserver-pro601";
+var _installdir = "jasperserver-pro61";
 var _username = "superuser";
 var _password = "superuser";
 var _debug = true;
